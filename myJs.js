c5d012c884c6aedb96322df8cb8e6f6cdd4ceb71
anupam-dixit <69384581+anupam-dixit@users.noreply.github.com>
2021-06-27 15:18:38 +0530

    Create myJs.js

---
+++ myJs.js
@@ -0,0 +1,79 @@
+function setCookie(key, value, expiry)
+{
+var expires = new Date();
+expires.setTime(expires.getTime() + (expiry * 24 * 60 * 60 * 1000));
+document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+
+function getCookie(key)
+{
+var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+return keyValue ? keyValue[2] : null;
+}
+
+function eraseCookie(key)
+{
+var keyValue = getCookie(key);
+setCookie(key, keyValue, '-1');
+}
+
+function taggerjs()
+    {
+      if(getCookie('ut')==""||getCookie('ut')==null)
+      {
+       // alert("setting");
+        setCookie('ut',''+Math.floor(Math.random() * 100000000) + 1,365 );
+      }
+      else
+      {
+       // alert("cookie already set");
+      }
+    }
+
+function random()
+{
+return Math.floor(100000000 + Math.random() * 900000000);
+}
+
+function demo()
+{
+	alert("demo");
+}
+
+var x = document.getElementById ("field_loc");
+
+function getLocation( )
+{
+  
+	if ( navigator.geolocation )
+	{
+		navigator.geolocation.getCurrentPosition (showPosition, showError);
+	}
+	else
+	{ 
+		x.value = "Geolocation is not supported by this browser.";
+	}
+}
+function showPosition( position )
+{
+	x.value = "" + position.coords.latitude + 
+		"," + position.coords.longitude;
+}
+function showError( error )
+{
+	switch ( error.code )
+	{
+		case error.PERMISSION_DENIED:
+			x.value = "0"
+			break;
+		case error.POSITION_UNAVAILABLE:
+			x.value = "1"
+			break;
+		case error.TIMEOUT:
+			x.value = "2"
+			break;
+		case error.UNKNOWN_ERROR:
+			x.value = "3"
+			break;
+	}
+}
